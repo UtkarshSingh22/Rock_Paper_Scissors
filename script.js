d48243e3f6eeec62f52b59a3664db82d562b4931
Utkarsh Singh <sutkarsh453@gmail.com>
2022-03-26 17:56:04 +0530

    Add play function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function computerPlay(){
+
     let chance = Math.floor(Math.random()*(3))+1;
     if(chance == 1)
         return 'rock';
@@ -12,3 +13,24 @@ function playerSelection(){
     let pChance = prompt('Enter your choice:');
     return pChance.toLowerCase();
 }
+
+function play(cMove,pMove){
+
+    if(cMove == 'rock' && pMove == 'scissor' || cMove == 'paper' && pMove == 'rock' || cMove == 'scissor' && pMove == 'paper'){
+        let res = 'You Lose! '+cMove+' beats '+pMove;
+        return res;
+    }
+    else if(pMove == 'rock' && cMove == 'scissor' || pMove == 'paper' && cMove == 'rock' || pMove == 'scissor' && cMove == 'paper'){
+        let res = 'You Won! '+pMove+' beats '+cMove;
+        return res;
+    } 
+    else{
+        let res = "It's a tie, you both got "+cMove;
+        return res;
+    } 
+}
+
+let cMove = computerPlay();
+let pMove = playerSelection();
+
+console.log(play());
