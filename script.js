75c18c9d903f0c9f7354d85bed354ab8a65489a0
Utkarsh Singh <sutkarsh453@gmail.com>
2022-04-04 14:16:44 +0530

    fix html and js saving problem

--- script.js
+++ script.js
@@ -1,76 +1,103 @@
-function computerPlay(){
-
-    let chance = Math.floor(Math.random()*(3))+1;
-    if(chance == 1)
-        return 'rock';
-    else if(chance == 2)
-        return 'paper';
-    else
-        return 'scissor';
-}
-
-function playerSelection(){
-    let pChance = prompt('Enter your choice:');
-    return pChance.toLowerCase();
-}
-
-function play(cMove,pMove){
-
-    let res;
-    if((cMove == 'rock' && pMove == 'scissor') || (cMove == 'paper' && pMove == 'rock') || (cMove == 'scissor' && pMove == 'paper')){
-        res = `You Lose! ${cMove} beats ${pMove}`;
-    }
-    else if((pMove == 'rock' && cMove == 'scissor') || (pMove == 'paper' && cMove == 'rock') || (pMove == 'scissor' && cMove == 'paper')){
-        res = `You Won! ${pMove} beats ${cMove}`;
-    } 
-    else{
-        res = `It's a tie, you both got ${cMove}`;
-    } 
-    return res;
-}
-
-function whoWins(cMove,pMove){
-
-    let res;
-    if((cMove == 'rock' && pMove == 'scissor') || (cMove == 'paper' && pMove == 'rock') || (cMove == 'scissor' && pMove == 'paper')){
-        res = 1;
-    }
-    else if((pMove == 'rock' && cMove == 'scissor') || (pMove == 'paper' && cMove == 'rock') || (pMove == 'scissor' && cMove == 'paper')){
-        res = 2;
-    }
-    else{
-        res = 0;
-    } 
-    return res;
-}
-
-function game(_cScore,_pScore){
-
-    while(_cScore < 5 && _pScore < 5){
-
-        let cMove = computerPlay();
-        let pMove = playerSelection();
-        console.log(play(cMove,pMove));
-
-        if(whoWins(cMove, pMove) == 1){
-            _cScore++;
-        }
-        else if(whoWins(cMove,pMove) == 2){
-            _pScore++;
-        }
-        
-        console.log('Computer Score: ', _cScore); 
-        console.log('Your Score: ',_pScore);
-    }
-    if(_cScore == 5)
-        console.log('You Lose!');
-    else{
-        console.log('Congratulations, You Won!!');
+function playerPlay(buttonID) {
+    switch (buttonID) {
+        case 'button-rock':
+            return 'Rock';
+        case 'button-paper':
+            return 'Paper';
+        case 'button-scissors':
+            return 'Scissors';
     }
 }
 
-let _pScore = 0, _cScore = 0;
+function computerPlay() {
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    return choice[Math.floor(Math.random() * 3)];
+}
 
-console.log(game(_cScore,_pScore));
+function playRound(event) {
+    let result;
+    const playerSelection = playerPlay(event.target.id);
+    const computerSelection = computerPlay();
 
+    if (playerSelection==='Rock') {
+        if (computerSelection==='Rock') result = 'Draw';
+        if (computerSelection==='Paper') result = 'Lose';
+        if (computerSelection==='Scissors') result = 'Win';
+    }
+    if (playerSelection==='Paper') {
+        if (computerSelection==='Rock') result = 'Win';
+        if (computerSelection==='Paper') result = 'Draw';
+        if (computerSelection==='Scissors') result = 'Lose';
+    }
+    if (playerSelection==='Scissors') {
+        if (computerSelection==='Rock') result = 'Lose';
+        if (computerSelection==='Paper') result = 'Win';
+        if (computerSelection==='Scissors') result = 'Draw';
+    }
+    
+    switch (result) {
+        case 'Win':
+            playerScore++;
+            resultText.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+            break;
+        case 'Lose':
+            computerScore++;
+            resultText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            break;
+        case 'Draw':
+            resultText.textContent = `It's a ${playerSelection} draw!`;
+            break;
 
+    }
+
+    scorePlayer.textContent = `Your Score: ${playerScore}`;
+    scoreComputer.textContent = `Computer's Score: ${computerScore}`;
+
+    if (playerScore===5 || computerScore===5) {
+        finishGame();
+    }
+}
+
+function initGame() {
+    btnRestart.style.display = 'none';
+    
+    playerScore = 0;
+    computerScore = 0;
+
+    winnerText.textContent = '';
+    scorePlayer.textContent = `Your Score: ${playerScore}`;
+    scoreComputer.textContent = `Computer's Score: ${computerScore}`;
+    resultText.textContent = '';
+
+    buttons.forEach(button => {
+        button.addEventListener('click', playRound);
+    });
+}
+
+function finishGame() {
+    buttons.forEach(button => {
+        button.removeEventListener('click', playRound);
+        button.style.pointerEvents = 'none';
+    });
+
+    if (playerScore===5) {
+        winnerText.textContent = 'You won!';
+    } else {
+        winnerText.textContent = 'You lost!';
+    }
+
+    btnRestart.style.display = 'block';
+}
+
+const winnerText = document.querySelector('#winner-text');
+const scorePlayer = document.querySelector('#score-player');
+const scoreComputer = document.querySelector('#score-computer');
+const buttons = document.querySelectorAll('.btn');
+const resultText = document.querySelector('#result-text');
+const btnRestart = document.querySelector('#btn-restart');
+
+let playerScore, computerScore;
+
+btnRestart.addEventListener('click', initGame);
+
+initGame();
