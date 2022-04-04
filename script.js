cae735676ef655de7745a4f3ddf6f7e034a93967
Utkarsh Singh <sutkarsh453@gmail.com>
2022-04-04 13:40:16 +0530

    from start

--- script.js
+++ script.js
@@ -1,114 +1,105 @@
-function computerPlay(){
-
-    let chance = Math.floor(Math.random()*(3))+1;
-    if(chance == 1)
-        return 'rock';
-    else if(chance == 2)
-        return 'paper';
-    else
-        return 'scissor';
+function playerPlay(buttonID) {
+    switch (buttonID) {
+        case 'button-rock':
+            return 'Rock';
+        case 'button-paper':
+            return 'Paper';
+        case 'button-scissors':
+            return 'Scissors';
+    }
 }
 
-function play(cMove,pMove){
-
-    let res;
-    if((cMove == 'rock' && pMove == 'scissor') || (cMove == 'paper' && pMove == 'rock') || (cMove == 'scissor' && pMove == 'paper')){
-        res = `You Lose! ${cMove} beats ${pMove}`;
-    }
-    else if((pMove == 'rock' && cMove == 'scissor') || (pMove == 'paper' && cMove == 'rock') || (pMove == 'scissor' && cMove == 'paper')){
-        res = `You Won! ${pMove} beats ${cMove}`;
-    } 
-    else{
-        res = `It's a tie, you both got ${cMove}`;
-    } 
-    return res;
+function computerPlay() {
+    const choice = ['Rock', 'Paper', 'Scissors'];
+    // return a random choice
+    return choice[Math.floor(Math.random() * 3)];
 }
 
-function whoWins(cMove,pMove){
+function playRound(event) {
+    let result;
+    const playerSelection = playerPlay(event.target.id);
+    const computerSelection = computerPlay();
 
-    let res;
-    if((cMove == 'rock' && pMove == 'scissor') || (cMove == 'paper' && pMove == 'rock') || (cMove == 'scissor' && pMove == 'paper')){
-        res = 1;
+    if (playerSelection==='Rock') {
+        if (computerSelection==='Rock') result = 'Draw';
+        if (computerSelection==='Paper') result = 'Lose';
+        if (computerSelection==='Scissors') result = 'Win';
     }
-    else if((pMove == 'rock' && cMove == 'scissor') || (pMove == 'paper' && cMove == 'rock') || (pMove == 'scissor' && cMove == 'paper')){
-        res = 2;
+    if (playerSelection==='Paper') {
+        if (computerSelection==='Rock') result = 'Win';
+        if (computerSelection==='Paper') result = 'Draw';
+        if (computerSelection==='Scissors') result = 'Lose';
     }
-    else{
-        res = 0;
-    } 
-    return res;
-}
-/*
-let _cScore = 0, _pScore = 0;
-
-while(_pScore < 5 && _cScore < 5){
-    let pChance = playerSelec();
-    let chance = computerPlay();
-    let res = play(chance, pChance);
+    if (playerSelection==='Scissors') {
+        if (computerSelection==='Rock') result = 'Lose';
+        if (computerSelection==='Paper') result = 'Win';
+        if (computerSelection==='Scissors') result = 'Draw';
+    }
     
-    const div = document.createElement('div');
-    div.className = 'result';
-    let text = document.createTextNode(res);
-    div.appendChild(text);
+    switch (result) {
+        case 'Win':
+            playerScore++;
+            resultText.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+            break;
+        case 'Lose':
+            computerScore++;
+            resultText.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+            break;
+        case 'Draw':
+            resultText.textContent = `It's a ${playerSelection} draw!`;
+            break;
 
-    const btnList = document.querySelector('.btns');
-    btnList.appendChild(div);
-}
-*/
+    }
 
+    scorePlayer.textContent = `You: ${playerScore}`;
+    scoreComputer.textContent = `CPU: ${computerScore}`;
 
-const btns = document.querySelector('button');
-
-btns.addEventListener('click', startGame);
-
-function startGame(e){
-
-    const head = document.querySelector('.btns');
-    head.removeChild(btns);
-
-    const but1 = document.createElement('button');
-    but1.id = 'rock';
-    but1.setAttribute('onClick','getId(this.id)')
-    but1.classList = 'butAdd';
-    but1.textContent = 'ROCK';
-    head.appendChild(but1);
-
-    const but2 = document.createElement('button');
-    but2.id = 'paper';
-    but2.setAttribute('onClick','getId(this.id)')
-    but2.classList = 'butAdd';
-    but2.textContent = 'PAPER';
-    head.appendChild(but2);
-
-    const but3 = document.createElement('button');
-    but3.id = 'scissor';
-    but3.setAttribute('onClick','getId(this.id)')
-    but3.classList = 'butAdd';
-    but3.textContent = 'SCISSORS';
-    head.appendChild(but3);
-
-    //let pScore = 0, cScore = 0;
-    //while(pScore < 5 && cScore < 5){
-
-        const pSel = '';
-        const press = document.querySelectorAll('.butAdd');
-        for(let i=0;i<press.length;i++){
-            press[i].addEventListener('click', getId);   
-        }
-        
-        function getId(e){
-            pSel = e.target.id;
-            return pSel;
-        }
-
-        getId()
-
-        console.log(pSel);
-    //}
-
-
+    if (playerScore===5 || computerScore===5) {
+        finishGame();
+    }
 }
 
+function initGame() {
+    btnRestart.style.display = 'none';
+    
+    playerScore = 0;
+    computerScore = 0;
 
+    winnerText.textContent = '';
+    scorePlayer.textContent = `You: ${playerScore}`;
+    scoreComputer.textContent = `CPU: ${computerScore}`;
+    resultText.textContent = '';
 
+    buttons.forEach(button => {
+        button.addEventListener('click', playRound);
+        button.style.pointerEvents = 'auto';
+    });
+}
 
+function finishGame() {
+    buttons.forEach(button => {
+        button.removeEventListener('click', playRound);
+        button.style.pointerEvents = 'none';
+    });
+
+    if (playerScore===5) {
+        winnerText.textContent = 'You won!';
+    } else {
+        winnerText.textContent = 'You lost!';
+    }
+
+    btnRestart.style.display = 'block';
+}
+
+const winnerText = document.querySelector('#winner-text');
+const scorePlayer = document.querySelector('#score-player');
+const scoreComputer = document.querySelector('#score-computer');
+const buttons = document.querySelectorAll('.btn');
+const resultText = document.querySelector('#result-text');
+const btnRestart = document.querySelector('#btn-restart');
+
+let playerScore, computerScore;
+
+btnRestart.addEventListener('click', initGame);
+
+initGame();
